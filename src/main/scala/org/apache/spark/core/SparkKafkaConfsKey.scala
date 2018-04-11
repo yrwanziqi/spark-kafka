4f9f71ce4234bf3254cef9799d4f01b6744052f4
LMQ <LMQ>
2018-04-11 14:23:07 +0800

    添加了几个配置key

--- src/main/scala/org/apache/spark/core/SparkKafkaConfsKey.scala
+++ src/main/scala/org/apache/spark/core/SparkKafkaConfsKey.scala
@@ -5,8 +5,13 @@ trait SparkKafkaConfsKey {
    * 如果groupid不存在或者过期选择从last还是从earliest开始
    */
    val WRONG_FROM="wrong.groupid.from"
+   val GROUPID="group.id"
+   val BROKER="metadata.broker.list"
+   val SERIALIZER="serializer.class"
    /*
     * 从last还是从consumer开始
     */
    val CONSUMER_FROM="kafka.consumer.from"
+   
+   val KAFKAOFFSET="kafka.offset"
 }
